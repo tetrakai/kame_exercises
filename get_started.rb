f7a04f825dbd8d0b635391e5a01c7fcec309a550
Karla Burnett <karla@stripe.com>
2015-03-31 13:17:45 -0700

    Fix getting started

--- get_started.rb
+++ get_started.rb
@@ -2,16 +2,4 @@ require 'kame'
 
 Kame.new do
   forward(50)
-
-  def square
-    forward(50)
-    turn_right(90)
-    forward(50)
-    turn_right(90)
-    forward(50)
-    turn_right(90)
-    forward(50)
-  end
-
-  square
 end
